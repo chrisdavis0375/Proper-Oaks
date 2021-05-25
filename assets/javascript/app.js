8e4066badec79b3c8a977de3fb5e396fe3d18fe3
Chris Davis <chrisdavis0375@gmail.com>
2021-05-25 00:34:19 -0500

    Starting files added, navbar finished, landing page started

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+console.log("Javascript working");
+
+$(".burger-icon").click(function menuToggle() {
+  console.log("Menu toggled.");
+  document.getElementById("burger").classList.toggle("nav-button-color");
+  if ($("#collapsedLinks").css("display") === "flex") {
+    $("#collapsedLinks").css("display", "none");
+    console.log("bitch");
+  } else {
+    $("#collapsedLinks").css("display", "flex");
+  }
+});

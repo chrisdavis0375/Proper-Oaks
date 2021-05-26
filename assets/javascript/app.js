b8c80bc1a4c47f8aebf92fbcf3b71d370e6f69a9
Chris Davis <chrisdavis0375@gmail.com>
2021-05-26 17:25:34 -0500

    Contact page started and styled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,18 @@
-console.log("Javascript working");
-
-$(".burger-icon").click(function menuToggle() {
-  console.log("Menu toggled.");
-  document.getElementById("burger").classList.toggle("nav-button-color");
-  if ($("#collapsedLinks").css("display") === "flex") {
-    $("#collapsedLinks").css("display", "none");
-    console.log("bitch");
-  } else {
-    $("#collapsedLinks").css("display", "flex");
-  }
-});
-window.onresize = function () {
+$(document).ready(function () {
+  console.log("Javascript working");
   $("#collapsedLinks").css("display", "none");
-};
+  $(".burger-icon").click(function menuToggle() {
+    console.log("Menu toggled.");
+    document.getElementById("burger").classList.toggle("nav-button-color");
+    if ($("#collapsedLinks").css("display") === "flex") {
+      $("#collapsedLinks").css("display", "none");
+      console.log("bitch");
+    } else {
+      $("#collapsedLinks").css("display", "flex");
+    }
+  });
+  window.onresize = function () {
+    $("#collapsedLinks").css("display", "none");
+    console.log("Window");
+  };
+});
